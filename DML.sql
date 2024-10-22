56033b665d34ffd72ededdc901407e05b7706908
labsfiap <labsfiap@PA501MICRO100.fiap.com.br>
2024-10-21 22:20:31 -0300

    H

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'RUA DA CONSOLACAO ',
+    4577,
+    '01308000',
+    'CONSOLACAO',
+    'SAO PAULO',
+    'BRASIL',
+    'METRO'
+);
